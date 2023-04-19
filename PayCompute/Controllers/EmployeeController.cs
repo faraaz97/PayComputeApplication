56b0d0a9d561a421cdd1e155a2d6e0c6173650da
Faraaz Khan <faraaz.khan@collabera.com>
2023-04-19 16:05:38 +0530

    Detail & Delete - EmployeeController

--- PayCompute/Controllers/EmployeeController.cs
+++ PayCompute/Controllers/EmployeeController.cs
@@ -160,10 +160,60 @@ namespace PayCompute.Controllers
                 }
                 await _employeeService.UpdateAsync(employee);
                 return RedirectToAction(nameof(Index));
-            }
-            
+            }            
             return View();
-
+        }
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            EmployeeDetailViewModel model = new EmployeeDetailViewModel()
+            {
+                Id = employee.Id,
+                EmployeeNo=employee.EmployeeNo,
+                FullName=employee.FullName,
+                Gender=employee.Gender,
+                DOB=employee.DOB,
+                DateJoined=employee.DateJoined,
+                Designation=employee.Designation,
+                NationalInsuranceNo=employee.NationalInsuranceNo,
+                Phone=employee.Phone,
+                Email=employee.Email,
+                PaymentMethod=employee.PaymentMethod,
+                StudentLoan=employee.StudentLoan,
+                UnionMember=employee.UnionMember,
+                Address=employee.Address,
+                City=employee.City,
+                ImageUrl=employee.ImageUrl,
+                PostCode=employee.PostCode
+            };
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var employee = _employeeService.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            var model = new EmployeeDeleteViewModel()
+            {
+                Id = employee.Id,
+                FullName = employee.FullName
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(EmployeeDeleteViewModel model)
+        {
+            await _employeeService.Delete(model.Id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
