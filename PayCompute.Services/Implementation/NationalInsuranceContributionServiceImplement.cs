dde909ec634ac460144b3e9db78a0a588bd7ed21
Faraaz Khan <faraaz.khan@collabera.com>
2023-04-26 14:14:28 +0530

    NICservice interface & NICservice implement

---
+++ PayCompute.Services/Implementation/NationalInsuranceContributionServiceImplement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCompute.Services.Implementation
+{
+    public class NationalInsuranceContributionServiceImplement : INationalInsuranceContributionService
+    {
+        private decimal NIRate;
+        private decimal NIC;
+        public decimal NIContribution(decimal totalAmount)
+        {
+            if (totalAmount < 719)
+            {
+                //lower earning limmit rate & below primary threshold
+                NIRate = .0m;
+                NIC = 0m;
+            }
+            else if (totalAmount >= 719 && totalAmount <= 4167)
+            {
+                //between primary threshold and upper earning limit (UEL)
+                NIRate = 1.2m;
+                NIC = ((totalAmount - 719) * NIRate);
+            }
+            else if (totalAmount > 4167)
+            {
+                //above upper earning limit(UEL)
+                NIRate = .02m;
+                NIC = ((4167 - 719) * .12m) + ((totalAmount - 4167) * NIRate);
+            }
+            return NIC;
+        }
+    }
+}
